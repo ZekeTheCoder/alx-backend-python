ea978075b10a4f12f48808778d63540996ba5121
Thabang Maphangisane <thabangh647@gmail.com>
2024-07-29 21:31:23 +0200

    5. Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" Parameterize and patch as decorators """
+""" Mocking a property """
 
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -18,10 +18,21 @@ class TestGithubOrgClient(TestCase):
     def test_org(self, org_name, mock):
         """ Test that GithubOrgClient.org returns the correct output """
         mock.return_value = {"name": org_name}
-        # Create an instance of GithubOrgClient
         client = GithubOrgClient(org_name)
         result = client.org
-        # Check that get_json was called exactly once with the expected URL
         mock.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
-        # Check that the returned result is as expected
         self.assertEqual(result, {"name": org_name})
+
+    def test_public_repos_url(self):
+        """Test the GithubOrgClient._public_repos_url function"""
+
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock,
+                          return_value={"repos_url": "Test value"}
+                          ) as mock_org:
+            mock_property = {"repos_url": "Test value"}
+            client = GithubOrgClient(mock_property.get("repos_url"))
+            result = client._public_repos_url
+
+            self.assertEqual(result, mock_org.return_value.get("repos_url"))
+            mock_org.assert_called_once
